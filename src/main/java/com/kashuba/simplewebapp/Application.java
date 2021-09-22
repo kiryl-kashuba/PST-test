de3fdfbc1786a6de87a02a76582257b630eccfec
Kiryl Kashuba <88317022+kiryl-kashuba-idf@users.noreply.github.com>
2021-09-22 11:47:13 +0300

    feat(app): change main

--- src/main/java/com/kashuba/simplewebapp/Application.java
+++ src/main/java/com/kashuba/simplewebapp/Application.java
@@ -3,21 +3,14 @@ package com.kashuba.simplewebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EntityScan("com.kashuba.simplewebapp")
 @SpringBootApplication
 @EnableSwagger2
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
     }
 }
